99d4e5f4cabc510de6a4c1530475fca0e2ee9577
Daniel Mutis <64042203+DanielMutis85@users.noreply.github.com>
2021-07-30 17:43:35 -0500

    Ajuste Contenidos

--- src/config/global.js
+++ src/config/global.js
@@ -84,7 +84,7 @@ export default {
         centro: 'Centro de Comercio y Servicios - Regional Tolima',
       },
       {
-        nombre: 'María Isabel Román Rueda',
+        nombre: 'Emilsen Alfonso Bautista',
         cargo: 'Validación de diseño y contenido',
         centro:
           'Centro Industrial del Diseño y la Manufactura Regional Santander',
--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -38,16 +38,16 @@ section
     .titulo__template--a.mb-4
       span.h4 1. Presentación
 
-    p.mb-0 AutoCAD es un software que está en la vida laboral de un profesional desde hace más de 30 años y es compatible con muchos formatos editables universales, permitiendo una interfaz de gráficos y un espacio ilimitado de trabajo como la creación de cotas, trazado de líneas y bocetos de cualquier índole. Este software se encuentra presente en diferentes disciplinas como la Arquitectura, la Ingeniería, el Diseño de interiores y el Diseño gráfico y publicitario por su amplio número de herramientas, lo que convierte a AutoCAD en una necesidad así como lo es aprender un segundo idioma para un profesional en diseño.
+    p.mb-0 AutoCAD es un #[i software] que está en la vida laboral de un profesional desde hace más de 30 años y es compatible con muchos formatos editables universales, permitiendo una interfaz de gráficos y un espacio ilimitado de trabajo como la creación de cotas, trazado de líneas y bocetos de cualquier índole. Este #[i software] se encuentra presente en diferentes disciplinas como la Arquitectura, la Ingeniería, el Diseño de interiores y el Diseño gráfico y publicitario por su amplio número de herramientas, lo que convierte a AutoCAD en una necesidad así como lo es aprender un segundo idioma para un profesional en diseño.
       br
       br
-      | Este programa de Diseño y planos en AutoCAD 2D está orientado a personas que trabajan en ramas de la Ingeniería, la Logística, la Arquitectura o la Construcción, las Telecomunicaciones, entre otras; con el fin de encontrar un curso que describa el uso del software de manera universal.
+      | Este programa de Diseño y planos en AutoCAD 2D está orientado a personas que trabajan en ramas de la Ingeniería, la Logística, la Arquitectura o la Construcción, las Telecomunicaciones, entre otras; con el fin de encontrar un curso que describa el uso del #[i software] de manera universal.
       br
       br
       | Durante la primera parte del curso se inicia con los principios básicos de dibujo técnico, algunas explicaciones de expresión gráfica, los diferentes tipos de diseño que puede elaborar un ingeniero o incluso un diseñador industrial o gráfico apoyado de la definición, aplicación e importancia del CAD en la industria.
       br
       br
-      | Adicionalmente, puede contar con la descripción de cada una de las herramientas de dibujo que se encuentran y permiten la elaboración de un ejercicio general, sin especializar el software en un área del conocimiento y que se puede aplicar a cualquier disciplina para la creación de bocetos de dibujos o planos 2D. 
+      | Adicionalmente, puede contar con la descripción de cada una de las herramientas de dibujo que se encuentran y permiten la elaboración de un ejercicio general, sin especializar el #[i software] en un área del conocimiento y que se puede aplicar a cualquier disciplina para la creación de bocetos de dibujos o planos 2D. 
       br
       br
       | Por último, se describirán las herramientas necesarias para la creación de ventanas gráficas para el apoyo y creación de documentos físicos de ingeniería o arquitectura como los planos y la importancia de formatos, tipos de cotas, directrices, partes de un plano básico e incluso la digitalización de documentos.
@@ -59,20 +59,20 @@ section
     .titulo__template--a.mb-4
       span.h4 2. Justificación<br>del programa
 
-    p.mb-0 En el informe de gestión 2018-2019 realizado por CAMACOL ofertó como su objeto principal de responsabilidad social y ambiental AutoCAD 2D como un software necesario para incentivar la economía por medio de la infraestructura y la construcción, la cual en el período de 2015-2019 ha licenciado 130.393 viviendas solo en la región comprendida entre Bogotá y Cundinamarca, las cuales necesariamente requieren del desarrollo de planimetrías, modelos y diseños CAD para poder iniciar su proceso constructivo.
+    p.mb-0 En el informe de gestión 2018-2019 realizado por CAMACOL ofertó como su objeto principal de responsabilidad social y ambiental AutoCAD 2D como un #[i software] necesario para incentivar la economía por medio de la infraestructura y la construcción, la cual en el período de 2015-2019 ha licenciado 130.393 viviendas solo en la región comprendida entre Bogotá y Cundinamarca, las cuales necesariamente requieren del desarrollo de planimetrías, modelos y diseños CAD para poder iniciar su proceso constructivo.
       br
       br
-      | Se determina la necesidad del manejo de un software como una herramienta que brinde resultados precisos y confiables sobre aproximaciones realistas para el prototipado de diseños; por esta razón, se considera que el software AutoCAD 2D ofrece las ventajas para ser competitivo en el medio, dando respuesta a las necesidades de distintos sectores económicos. Los resultados mencionados buscan promover conocimientos y habilidades en los aprendices para elaborar y modificar diseños, vistas y planos mediante el AutoCAD 2D, aplicando los conocimientos adquiridos y demostrando la capacidad de interpretar un plano en su estructura básica, junto con la creación de dibujos precisos a escala según su rama del conocimiento.
+      | Se determina la necesidad del manejo de un #[i software] como una herramienta que brinde resultados precisos y confiables sobre aproximaciones realistas para el prototipado de diseños; por esta razón, se considera que el #[i software] AutoCAD 2D ofrece las ventajas para ser competitivo en el medio, dando respuesta a las necesidades de distintos sectores económicos. Los resultados mencionados buscan promover conocimientos y habilidades en los aprendices para elaborar y modificar diseños, vistas y planos mediante el AutoCAD 2D, aplicando los conocimientos adquiridos y demostrando la capacidad de interpretar un plano en su estructura básica, junto con la creación de dibujos precisos a escala según su rama del conocimiento.
       br
       br
-      | Según Arellano Núñez (2018) "es de vital importancia mencionar que este software se utiliza en todos los ámbitos científico, creativo, artístico, y se ha venido transformando en una herramienta básica en las universidades, siendo así que aquellos estudiantes que la conozcan tengan claro el uso y las aplicaciones de AutoCAD, así tendrán grandes oportunidades de crecer y evolucionar en el área de la Ingeniera, por consiguiente se tienen ejemplos donde mediante este software se han creado grandes diseños ingenieriles" (p. 6).
+      | Según Arellano Núñez (2018) "es de vital importancia mencionar que este #[i software] se utiliza en todos los ámbitos científico, creativo, artístico, y se ha venido transformando en una herramienta básica en las universidades, siendo así que aquellos estudiantes que la conozcan tengan claro el uso y las aplicaciones de AutoCAD, así tendrán grandes oportunidades de crecer y evolucionar en el área de la Ingeniera, por consiguiente se tienen ejemplos donde mediante este #[i software] se han creado grandes diseños ingenieriles" (p. 6).
 
 
   .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
     .titulo__template--a.mb-4
       span.h4 3. Población <br>objetivo
 
-    p.mb-0 El curso de Diseño y planos en AutoCAD 2D está dirigido a jóvenes y adultos en todo el territorio nacional, interesados en adquirir conocimientos y habilidades técnicas para ser aplicadas en procesos de diseño de cualquier elemento en áreas de la Ingeniería, la Logística, la Arquitectura o la Construcción y, las Telecomunicaciones, etc., por medio de la descripción de herramientas y funciones universales del software. 
+    p.mb-0 El curso de Diseño y planos en AutoCAD 2D está dirigido a jóvenes y adultos en todo el territorio nacional, interesados en adquirir conocimientos y habilidades técnicas para ser aplicadas en procesos de diseño de cualquier elemento en áreas de la Ingeniería, la Logística, la Arquitectura o la Construcción y, las Telecomunicaciones, etc., por medio de la descripción de herramientas y funciones universales del #[i software]. 
 
 
   .container.tarjeta.tarjeta__template--azul-claro.p-4.p-md-5.mb-5(data-aos="fade-up")
